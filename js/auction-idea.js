6189214bd80adeaf5576f2970b3696ee9b860f54
Farah <farah.hsh.12@gmail.com>
2023-09-25 18:48:53 +0300

    almost done

--- js/auction-idea.js
+++ js/auction-idea.js
@@ -1,79 +1,67 @@
-// API 
-const ideaData= (id,name, description, patentType, patentCountries)=>{
-    axios.get(`https://65082eef56db83a34d9be320.mockapi.io/IDEAS/${id}`,{ name, description, patentType, patentCountries}).then((response)=> {
-    const api= response.data;
-    const apiTable= document.getElementById("data-list");
+const ideaData = JSON.parse(localStorage.getItem('ideaData'));
 
-
-        const row= document.createElement("tr");
-        row.innerHTML=`
-        <td>
-            <div class="card">
-                <div class="infos">
-                    <div class="info">
-                        <div>
-                             <p class="name">${api.name}</p>
-                             <p class="function">${api.description}</p>
-                        </div>
-                            <div class="stats">
-                                 <p class="flex flex-col">نوع الملكية الفكرية <span class="state-value">${api.patentType}/${api.patentCountries}</span> </p>
-                                 <p class="flex">الحد الأعلى للمزاد <span class="state-value"> 1,000,000,000 ر.س. </span> </p>     
-                            </div>
-                    </div>
-                </div>
-                <div class=container>
-                    <input  class="box" placeholder="اضف مزايدتك">
-                    <button class="request" type="button" >زايد الأن </button>
-                </div>
-                <ul id="listAuction"></ul>
-            </div>
-         </td>
-        `
-        apiTable.appendChild(row);  
- 
-
-
-});
-   
-
+if (ideaData) {
+    document.getElementById('idea_name').innerHTML = ideaData.name;
+    document.getElementById('idea_description').innerHTML = ideaData.description;
+    document.getElementById('idea_patentType').innerHTML = ideaData.patentTypes;
+} else {
+    document.getElementById('idea-details').innerHTML = 'No idea data available.';
 }
-    
+const button = document.querySelector('.request');
+let auctionValue = document.getElementsByClassName("box");
+const listAuction=document.getElementById("listAuction");
 
 
+button.addEventListener("click", function(){
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "لقد تم إضافة مزايدتك بنجاح ",
+        showConfirmButton: false,
+        timer: 3000,
+      });
 
-   
+        const listAuction= document.getElementById("listAuction");
+        if(auctionValue.value < 1000000000){
+           
+            let output= auctionValue.value;
 
+            let list = document.createElement("li");
 
+            list.innerText= output;
+            listAuction.appendChild(output);
+            auctionValue.value= '';
+           
+        }else{
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "الف مبروك! ربحت المزاد",
+                showConfirmButton: false,
+                timer: 3000,
+              });
+          
+           
+        }
 
+})
 
+// addValue = () =>{
+//     let auctionValue = document.getElementsByClassName("box");
+//     const listAuction= document.getElementById("listAuction");
+//     if(auctionValue.value < "1,000,000,000"){
+//         let list = document.createElement("li");
+//         list.innerText= auctionValue.value;
+//     }else{
+//         Swal.fire({
+//             position: 'center',
+//             icon: 'sucess',
+//             title: 'مبروك! لقد ربحت المزاد',
+//             showConfirmButton: false,
+//             timer: 1500
+//           })
+//     }
+//     listAuction.appendChild(list);
 
-   
+// }
 
-
-
-// axios.get(`https://65082eef56db83a34d9be320.mockapi.io/IDEAS/${id}`).then((re) => {
-//     const row= document.createElement("tr");
-//         row.innerHTML=`
-//         <td>
-//             <div class="card">
-//                 <div class="infos">
-//                     <div class="info">
-//                         <div>
-//                              <p class="name">${name}</p>
-//                              <p class="function">${description}</p>
-//                         </div>
-//                             <div class="stats">
-//                                  <p class="flex flex-col">نوع الملكية الفكرية <span class="state-value">${patentType}/${patentCountries}</span> </p>
-//                                  <p class="flex">الحد الأعلى للمزاد <span class="state-value"> 10,000,000 ر.س. </span> </p>     
-//                             </div>
-//                     </div>
-//                 </div>
-//                 <div class=container>
-//                     <input  class="box" placeholder="اضف مزايدتك">
-//                     <button class="request" type="button">زايد الأن </button>
-//                 </div>
-//             </div>
-//          </td>
-//         `
-//         apiTable.appendChild(row);  
-//   });
