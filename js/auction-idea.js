50a0555a986ee0d4548000b0c66c74f82dbf1804
Farah <farah.hsh.12@gmail.com>
2023-09-26 16:35:04 +0300

    change

--- js/auction-idea.js
+++ js/auction-idea.js
@@ -17,7 +17,7 @@ button.addEventListener("click", addValue);
 function addValue(e){
     e.preventDefault();
     if(auctionValue.value != 1000000000){
-        let value = auctionValue.value;
+        const value = auctionValue.value;
         
  
         let newItem = document.createElement("li");
