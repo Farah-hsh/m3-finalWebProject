de4ec0403c2c01363474703af495f8202a85b377
Farah <farah.hsh.12@gmail.com>
2023-09-26 21:15:54 +0300

    auction listing value almost done

--- js/auction-idea.js
+++ js/auction-idea.js
@@ -8,45 +8,52 @@ if (ideaData) {
     document.getElementById('idea-details').innerHTML = 'No idea data available.';
 }
 
-// let auctionValue = document.getElementsByClassName("box");
+let auctionValue = document.getElementsByClassName("box");
 const button = document.getElementById('request');
-// const listAuction=document.getElementById("listAuction");
-button.addEventListener("click",function(){
-    Swal.fire({
-                            position: "center",
-                            icon: "success",
-                            title: "الف مبروك! ربحت المزاد",
-                            showConfirmButton: false,
-                            timer: 3000,
-                          });
-        
+const listAuction=document.getElementById("listAuction");
 
+
+button.addEventListener("click", function(){
+    if(auctionValue.value != 1000000000){
+        const value = auctionValue.value;
+        
+ 
+        let newItem = document.createElement("li");
+        newItem.id = "newvalue" ;
+ 
+        
+ 
+        newItem.innerHTML = `
+        <div class="auction">
+            <div class="icon"><i class="fa-solid fa-circle-user fa-2x" style="color: #2f5d8c;"></i></div>
+            <div class="textBox">
+                <div class="textContent">
+                    <p class="h1">${value}</p>
+                       <span class="span">الأن</span>
+               </div>
+           <div>
+        </div>
+        `;
+        listAuction.appendChild(newItem);
+        Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "لقد تمت إضافة مزايدتك بنجاح! ترقبنا على بريدك الالكتروني لمعرفة من الرابح",
+                        showConfirmButton: false,
+                        timer: 6000,
+            });
+ 
+    }else{
+        Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "الف مبروك! ربحت المزاد",
+                    showConfirmButton: false,
+                    timer: 6000,
+                  });
+
+
+    }
 })
-
-// button.addEventListener("click", function(){
-//     if(auctionValue.value != 1000000000){
-//         const value = auctionValue.value;
-        
- 
-//         let newItem = document.createElement("li");
-//         newItem.id = "newvalue" ;
- 
-        
- 
-//         newItem.innerText = value;
-//         listAuction.appendChild(newItem);
- 
-//     }else{
-//         Swal.fire({
-//                     position: "center",
-//                     icon: "success",
-//                     title: "الف مبروك! ربحت المزاد",
-//                     showConfirmButton: false,
-//                     timer: 3000,
-//                   });
-
-
-//     }
-// })
 
 
